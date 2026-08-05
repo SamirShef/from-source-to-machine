37c478d00abadb21d09042c438d090b5536a8272
SamirShef <ssserin10@gmail.com>
2026-08-05 17:12:46 +0300

    removed default constructor in Token

--- compiler/include/pebble/lexer/token.h
+++ compiler/include/pebble/lexer/token.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "pebble/basic/pos.h"
 #include "pebble/diagnostic/span.h"
 #include "pebble/lexer/token_kind.h"
 #include <string_view>
@@ -13,11 +12,6 @@ struct Token {
 
     Token (TokenKind kind, std::string_view val, diagnostic::Span span)
         : Kind (kind), Val (val), Span (span) {}
-
-    Token ()
-        : Kind (TokenKind::Unknown),
-          Val (std::string_view ("")),
-          Span (basic::Pos{ 0, 0 }, basic::Pos{ 0, 0 }) {}
 };
 
 }
